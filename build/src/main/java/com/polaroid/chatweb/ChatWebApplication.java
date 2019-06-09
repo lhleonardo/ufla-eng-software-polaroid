bb0a3baa2e6b39741e3514878352c16b0f9b850f
Leonardo Braz <lhleonardo@hotmail.com>
2019-06-09 05:03:18 -0300

    Modificando tela para acessar resources

--- build/src/main/java/com/polaroid/chatweb/ChatWebApplication.java
+++ build/src/main/java/com/polaroid/chatweb/ChatWebApplication.java
@@ -2,11 +2,13 @@ package com.polaroid.chatweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class ChatWebApplication {
 
 	public static void main(String[] args) {
+		System.out.println(new BCryptPasswordEncoder().encode("123"));
 		SpringApplication.run(ChatWebApplication.class, args);
 	}
 
--- build/src/main/java/com/polaroid/chatweb/model/User.java
+++ build/src/main/java/com/polaroid/chatweb/model/User.java
@@ -1,5 +1,8 @@
 package com.polaroid.chatweb.model;
 
+import java.util.Collection;
+import java.util.Collections;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,8 +11,14 @@ import javax.persistence.Id;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 @Entity
-public class User {
+public class User implements UserDetails {
+
+	// need this constant for serialize within session's 
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,7 +26,7 @@ public class User {
 
 	@Column(unique = true)
 	@NotNull
-	private String nickname;
+	private String username;
 
 	@Column(unique = true)
 	@Email
@@ -35,11 +44,11 @@ public class User {
 
 	}
 
-	public User(Long id, @NotNull String nickname, @Email @NotNull String email, @NotNull String ownerName,
+	public User(Long id, @NotNull String username, @Email @NotNull String email, @NotNull String ownerName,
 			String password) {
 		super();
 		this.id = id;
-		this.nickname = nickname;
+		this.username = username;
 		this.email = email;
 		this.ownerName = ownerName;
 		this.password = password;
@@ -47,10 +56,6 @@ public class User {
 
 	public Long getId() {
 		return id;
-	}
-
-	public String getNickname() {
-		return nickname;
 	}
 
 	public String getEmail() {
@@ -63,6 +68,36 @@ public class User {
 
 	public String getPassword() {
 		return password;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return Collections.emptyList();
+	}
+
+	@Override
+	public String getUsername() {
+		return this.username;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return false;
 	}
 
 }
