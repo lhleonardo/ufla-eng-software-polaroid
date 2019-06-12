eaac9234b57666a39e4eb1e546800b9d3433461f
Leonardo Braz <lhleonardo@hotmail.com>
2019-06-12 13:35:06 -0300

    Comentários de código.

--- build/src/main/java/com/polaroid/chatweb/controller/UserController.java
+++ build/src/main/java/com/polaroid/chatweb/controller/UserController.java
@@ -6,8 +6,6 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
